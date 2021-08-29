c8360b9a032b9eff1a519a2c83f4838af32d5f3a
Ana Vassallo <aniavassallo@gmail.com>
2021-08-29 03:37:21 -0500

    ¿Cómo ser una persona exitosa?

---
+++ js/PersonaExitoDynamic.js
@@ -0,0 +1,227 @@
+
+var puntaje = 0;
+
+// TRABAJO
+function comprobarb2() {
+    b2 = document.getElementById('b2').style.backgroundColor = "red";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarb3() {
+    b3 = document.getElementById('b3').style.backgroundColor = "red";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarb4() {
+    b4 = document.getElementById('b4').style.backgroundColor = "red";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarb5() {
+    b5 = document.getElementById('b5').style.backgroundColor = "red";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarb6() {
+    b6 = document.getElementById('b6').style.backgroundColor = "red";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarb7() {
+    b7 = document.getElementById('b7').style.backgroundColor = "red";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarb8() {
+    b8 = document.getElementById('b8').style.backgroundColor = "red";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+// ORDEN
+
+function comprobarc7() {
+    c7 = document.getElementById('c7').style.backgroundColor = "gold";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobard6() {
+    d6 = document.getElementById('d6').style.backgroundColor = "gold";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobare5() {
+    e5 = document.getElementById('e5').style.backgroundColor = "gold";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarf4() {
+    f4 = document.getElementById('f4').style.backgroundColor = "gold";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+// LIMPIEZA
+
+function comprobarc9() {
+    c9 = document.getElementById('c9').style.backgroundColor = "pink";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobard9() {
+    d9 = document.getElementById('d9').style.backgroundColor = "pink";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobare9() {
+    e9 = document.getElementById('e9').style.backgroundColor = "pink";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarf9() {
+    f9 = document.getElementById('f9').style.backgroundColor = "pink";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarg9() {
+    g9 = document.getElementById('g9').style.backgroundColor = "pink";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarh9() {
+    h9 = document.getElementById('h9').style.backgroundColor = "pink";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobari9() {
+    i9 = document.getElementById('i9').style.backgroundColor = "pink";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarj9() {
+    j9 = document.getElementById('j9').style.backgroundColor = "pink";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+// LIDER
+
+function comprobard3() {
+    d3 = document.getElementById('d3').style.backgroundColor = "green";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobare4() {
+    e4 = document.getElementById('e4').style.backgroundColor = "green";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarf5() {
+    f5 = document.getElementById('f5').style.backgroundColor = "green";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarg6() {
+    g6 = document.getElementById('g6').style.backgroundColor = "green";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobarh7() {
+    h7 = document.getElementById('h7').style.backgroundColor = "green";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+// RIESGO
+
+function comprobari2() {
+    i2 = document.getElementById('i2').style.backgroundColor = "blue";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobari3() {
+    i3 = document.getElementById('i3').style.backgroundColor = "blue";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobari4() {
+    i4 = document.getElementById('i4').style.backgroundColor = "blue";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobari5() {
+    i5 = document.getElementById('i5').style.backgroundColor = "blue";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobari6() {
+    i6 = document.getElementById('i6').style.backgroundColor = "blue";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function comprobari7() {
+    i7 = document.getElementById('i7').style.backgroundColor = "blue";
+    puntaje++;
+    console.log(puntaje);
+    ganar();
+}
+
+function ganar() {
+    if(puntaje == 30) {
+        alert("¡Felicidades, has ganado!");
+    }
+}
--- js/PersonaExitoEvaluacion.js
+++ js/PersonaExitoEvaluacion.js
@@ -25,6 +25,18 @@ var calificacion = 0;
     // #6
     var correcto_6 = correcto.cloneNode(true);
 
+    // #7
+    var correcto_7 = correcto.cloneNode(true);
+
+    // #8
+    var correcto_8 = correcto.cloneNode(true);
+
+    // #9
+    var correcto_9 = correcto.cloneNode(true);
+
+    // #10
+    var correcto_10 = correcto.cloneNode(true);
+
 // Respuestas Incorrectas 
 
     // #1
@@ -33,90 +45,145 @@ var calificacion = 0;
     incorrecto.classList.add("alert-danger");
     incorrecto.style.width = "100%";
     incorrecto.style.marginTop = "20px";
-    incorrecto.textContent = "¡Oh no! Tu respuesta es incorrecta. Recuerda que las personas proactivas asumen la responsabilidad y se eligen su propia actitud y respuesta ante las circunstancias.";
+    incorrecto.textContent = "¡Oh no! Tu respuesta es incorrecta. Recuerda que una persona con liderazgo sabe trabajar individualmente y en equipo para crear y fomentar ideas.";
 
     // #2
     var incorrecto_2 = incorrecto.cloneNode(true);
-    incorrecto_2.textContent = "¡Oh no! Tu respuesta es incorrecta. Las personas deben tener una misión personal para programar su futuro, ya que de esta manera la persona puede enfocarse en sus pasiones, talentos, sueños y deseos.";
+    incorrecto_2.textContent = "¡Oh no! Tu respuesta es incorrecta. Generalmente los líderes toman riesgos y dan pasos repentinos para alcanzar sus metas.";
 
     // #3
     var incorrecto_3 = incorrecto.cloneNode(true);
-    incorrecto_3.textContent = "¡Oh no! Tu respuesta es incorrecta. Es importante escuchar para poder comprender diversos puntos de vista y ser empáticos con las personas.";
+    incorrecto_3.textContent = "¡Oh no! Tu respuesta es incorrecta. Las personas exitosas aprovechan al máximo su tiempo. Ya sea ratos libres, entre labores o los fines de semana, el tiempo es oro para ellas.";
 
     // #4
     var incorrecto_4 = incorrecto.cloneNode(true);
-    incorrecto_4.textContent = "¡Oh no! Tu respuesta es incorrecta. La comunicación es importante para poder escuchar, reflexionar, responder y cooperar en un equipo.";
+    incorrecto_4.textContent = "¡Oh no! Tu respuesta es incorrecta. Los líderes tienen mente abierta para poder escuchar a su equipo y a las personas que los rodean y así poder tomar decisiones oportunas.";
     
     // #5
     var incorrecto_5 = incorrecto.cloneNode(true);
-    incorrecto_5.textContent = "¡Oh no! Tu respuesta es incorrecta. Recuerda que los 4 aspectos importantes del 7mo hábito son: cuerpo, espiritual, cognitivo y emocional.";
+    incorrecto_5.textContent = "¡Oh no! Tu respuesta es incorrecta. Recuerda que las personas exitosas siempre tienen hambre de conocimiento y desean aprender cada día algo. El conocimiento es oro para ellas y les ayuda a tener una visión más amplia del mundo y de las personas que las rodean.";
 
     // #6
     var incorrecto_6 = incorrecto.cloneNode(true);
-    incorrecto_6.textContent = "¡Oh no! Tu respuesta es incorrecta. Stephen Covey fue el escritor del libro 'Los 7 hábitos de la gente altamente efectiva' en el año de 1989.";
+    incorrecto_6.textContent = "¡Oh no! Tu respuesta es incorrecta. La paciencia es una virtud que ayuda a los líderes a tomar decisiones con calma y sabiduría. A veces el pensar dos veces sobre una decisión ayuda a que no se cometan errores catastróficos irreparables.";
+
+    // #7
+    var incorrecto_7 = incorrecto.cloneNode(true);
+    incorrecto_7.textContent = "¡Oh no! Tu respuesta es incorrecta. Un lider debe tener una actitud positiva y ver siempre con buena cara hacia el futuro. La negatividad no ayudará en nada, sólo hará que el equipo esté desmotivado y desanimado.";
+
+    // #8
+    var incorrecto_8 = incorrecto.cloneNode(true);
+    incorrecto_8.textContent = "¡Oh no! Tu respuesta es incorrecta. Recuerda que una persona exitosa debe ser persistente ante las metas que quiera alcanzar. Querer es poder. Si un lider no pone esfuerzo y ganas de su parte, no podrá alcanzar las metas preestablecidas. Las metas se obtienen con mucho trabajo, perseverancia y dedicación.";
+
+    // #9
+    var incorrecto_9 = incorrecto.cloneNode(true);
+    incorrecto_9.textContent = "¡Oh no! Tu respuesta es incorrecta. La comunicación es la clave de buenas decisiones. Si no se tiene una buena comunicación entre los miembros del equipo, las ideas y metas podrían ser implementadas incorrectamente y generar resultados confusos.";
+
+    // #10
+    var incorrecto_10 = incorrecto.cloneNode(true);
+    incorrecto_10.textContent = "¡Oh no! Tu respuesta es incorrecta. Las personas exitosas deben generar confianza entre la población para poder recibir apoyo de ésta.";
 
     // Función de Calificar 
     function submit() {
         // Pregunta #1
-        if ($('#responsabilidad').is(':checked')) {
+        if (document.getElementById('pregunta1').value == 'equipo') {
             document.getElementById("retroalimentacion").appendChild(correcto);
             document.getElementById("retroalimentacion").classList.add("w-100");
             calificacion++;
-        } else if ($('#enojo').is(':checked') || $('#pereza').is(':checked') || $('#negatividad').is(':checked')) {
+        } else {
             document.getElementById("retroalimentacion").appendChild(incorrecto);
             document.getElementById("retroalimentacion").classList.add("w-100");
         }
 
         // Pregunta #2
-        if ($('#mision').is(':checked')) {
+        if (document.getElementById('pregunta2').value == 'arriesgada') {
             document.getElementById("retroalimentacion_2").appendChild(correcto_2);
             document.getElementById("retroalimentacion_2").classList.add("w-100");
             calificacion++;
-        } else if ($('#flojera').is(':checked') || $('#odio').is(':checked') || $('#empeno').is(':checked')) {
+        } else {
             document.getElementById("retroalimentacion_2").appendChild(incorrecto_2);
             document.getElementById("retroalimentacion_2").classList.add("w-100");
         }
 
         // Pregunta #3
-        if ($('#escuchar').is(':checked')) {
+        if (document.getElementById('pregunta3').value == 'tiempo') {
             document.getElementById("retroalimentacion_3").appendChild(correcto_3);
             document.getElementById("retroalimentacion_3").classList.add("w-100");
             calificacion++;
-        } else if ($('#saltar').is(':checked') || $('#gritar').is(':checked') || $('#ignorar').is(':checked')) {
+        } else {
             document.getElementById("retroalimentacion_3").appendChild(incorrecto_3);
             document.getElementById("retroalimentacion_3").classList.add("w-100");
         }
 
         // Pregunta #4
-        if ($('#comunicacion').is(':checked')) {
+        if (document.getElementById('pregunta4').value == 'abierta') {
             document.getElementById("retroalimentacion_4").appendChild(correcto_4);
             document.getElementById("retroalimentacion_4").classList.add("w-100");
             calificacion++;
-        } else if ($('#peleas').is(':checked') || $('#inasistencia').is(':checked') || $('#distracciones').is(':checked')) {
+        } else {
             document.getElementById("retroalimentacion_4").appendChild(incorrecto_4);
             document.getElementById("retroalimentacion_4").classList.add("w-100");
         }
 
         // Pregunta #5
-        if ($('#emocional').is(':checked')) {
+        if (document.getElementById('pregunta5').value == 'conocimiento') {
             document.getElementById("retroalimentacion_5").appendChild(correcto_5);
             document.getElementById("retroalimentacion_5").classList.add("w-100");
             calificacion++;
-        } else if ($('#hambre').is(':checked') || $('#olvido').is(':checked') || $('#confusion').is(':checked')) {
+        } else {
             document.getElementById("retroalimentacion_5").appendChild(incorrecto_5);
             document.getElementById("retroalimentacion_5").classList.add("w-100");
         }
 
         // Pregunta #6
-        if ($('#stephen').is(':checked')) {
+        if (document.getElementById('pregunta6').value == 'paciencia') {
             document.getElementById("retroalimentacion_6").appendChild(correcto_6);
             document.getElementById("retroalimentacion_6").classList.add("w-100");
             calificacion++;
-        } else if ($('#bill').is(':checked') || $('#carlos').is(':checked') || $('#hawking').is(':checked')) {
+        } else {
             document.getElementById("retroalimentacion_6").appendChild(incorrecto_6);
             document.getElementById("retroalimentacion_6").classList.add("w-100");
         }
 
+        // Pregunta #7
+        if (document.getElementById('pregunta7').value == 'positiva') {
+            document.getElementById("retroalimentacion_7").appendChild(correcto_7);
+            document.getElementById("retroalimentacion_7").classList.add("w-100");
+            calificacion++;
+        } else {
+            document.getElementById("retroalimentacion_7").appendChild(incorrecto_7);
+            document.getElementById("retroalimentacion_7").classList.add("w-100");
+        }
+
+        // Pregunta #8
+        if (document.getElementById('pregunta8').value == 'persistente') {
+            document.getElementById("retroalimentacion_8").appendChild(correcto_8);
+            document.getElementById("retroalimentacion_8").classList.add("w-100");
+            calificacion++;
+        } else {
+            document.getElementById("retroalimentacion_8").appendChild(incorrecto_8);
+            document.getElementById("retroalimentacion_8").classList.add("w-100");
+        }
+
+        // Pregunta #9
+        if (document.getElementById('pregunta9').value == 'comunicacion') {
+            document.getElementById("retroalimentacion_9").appendChild(correcto_9);
+            document.getElementById("retroalimentacion_9").classList.add("w-100");
+            calificacion++;
+        } else {
+            document.getElementById("retroalimentacion_9").appendChild(incorrecto_9);
+            document.getElementById("retroalimentacion_9").classList.add("w-100");
+        }
+
+        // Pregunta #10
+        if (document.getElementById('pregunta10').value == 'confianza') {
+            document.getElementById("retroalimentacion_10").appendChild(correcto_10);
+            document.getElementById("retroalimentacion_10").classList.add("w-100");
+            calificacion++;
+        } else {
+            document.getElementById("retroalimentacion_10").appendChild(incorrecto_10);
+            document.getElementById("retroalimentacion_10").classList.add("w-100");
+        }
         
         document.getElementById("calificacion").textContent = calificacion;
 
